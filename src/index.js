cc790f0056fdc7269b9d9fb7f51e63077b7a6ad2
kjoscha <kjoscha@gmail.com>
2019-07-16 20:32:38 +0200

    add map to messages + move message into component

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import 'semantic-ui-css/semantic.min.css'
+import '../leaflet.config.js';
 
 ReactDOM.render(<App/>, document.getElementById('root'))
