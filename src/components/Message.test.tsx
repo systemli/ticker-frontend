056c9144bff9b6dd3dcd678c88910f8b45599dbe
louis <louis@systemli.org>
2022-04-02 14:50:56 +0200

    ✅ Add Test for Message

---
+++ src/components/Message.test.tsx
@@ -0,0 +1,40 @@
+import { render, screen } from '@testing-library/react'
+import React from 'react'
+import Message from './Message'
+
+describe('Message', function () {
+  test('renders correctly', function () {
+    const message = {
+      id: '1',
+      text: 'message',
+      ticker: 1,
+      creation_date: new Date(),
+      tweet_id: '',
+      tweet_user: '',
+      attachments: [],
+      geo_information: '{"type":"FeatureCollection","features":[]}',
+    }
+    const { asFragment } = render(<Message message={message} />)
+
+    expect(asFragment()).toMatchSnapshot()
+
+    expect(screen.getByText('a few seconds ago')).toBeInTheDocument()
+  })
+
+  test('renders with map correctly', function () {
+    const message = {
+      id: '1',
+      text: 'message',
+      ticker: 1,
+      creation_date: new Date(),
+      tweet_id: '',
+      tweet_user: '',
+      attachments: [],
+      geo_information:
+        '{"type":"FeatureCollection","features":[{"type":"Feature","geometry":{"type":"Point","coordinates":[13.466282,52.5024]},"properties":null}]}',
+    }
+    const { asFragment } = render(<Message message={message} />)
+
+    expect(asFragment()).toMatchSnapshot()
+  })
+})
