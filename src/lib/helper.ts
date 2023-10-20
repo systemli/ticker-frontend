7c122dc72e412e1d376d3c5a37df0c629b83c8d8
louis <louis@systemli.org>
2023-10-20 22:10:39 +0200

    🔥 Remove replaceMagic function

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -6,16 +6,3 @@ export const isMobile = (): boolean => {
   const width = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth
   return width < breakpoints.mobile
 }
-
-// FIXME: Might be better to use a library like validator.js
-// to catch more cases.
-export const replaceMagic = (text: string): string => {
-  return text
-    ? text
-        .replace(/(https?:\/\/([a-zA-Z0-9._\-/]+))/g, '<a href="$1" target="_blank" rel="noopener noreferrer">$2</a>')
-        .replace(/#(\S+)/g, '<a target="_blank" rel="noopener noreferrer" href="https://twitter.com/search?q=%23$1">#$1</a>')
-        .replace(/ @(\S+)/g, ' <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/$1">@$1</a>')
-        .replace(/([a-zA-Z0-9_\-.]+@[a-zA-Z0-9_-]+.[a-zA-Z]+)/g, '<a href="mailto:$1">$1</a>')
-        .replace(/(?:\r\n|\r|\n)/g, '<br/>')
-    : ''
-}
